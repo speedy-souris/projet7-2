b48d66e5cdafbb4445f016640784adc553ec3a5d
Pascal MACCHI <pascal.macchi@gmail.com>
2021-07-06 22:07:20 +0200

    GrandpyRobot module
    import frozendict (to have dictionary key value immutable)
    
    Creation class GrandpyRobot
    
    Creation static method grandpy_data_of_message
    returns the desired value frozendict according to the key used
    
    T.D.D grandpyRobot
    
    Class TestGrandpyRobot
    
    test_grandpy_data_message method
    expected message ==> New user question ('user_question')
    expected message ==> response from grandpy ('response')
    expected message ==> memory weakness ('tired')
    
    test_grandpy_incorrect_message method creation
    expected message ==> incomprehension of the user ('incomprehension')
    expected message ==> user inpoliteness ('mannerless')
    expected message ==> lack of respect for the user ('disrespectful')
    
    test_grandpy_limit_message method message creation
    expected message ==> incivility limit reached ('incivility_limit')
    expected message ==> indecency limit reached ('indecency_limit')
    expected message ==> incomprehension limit reached ('incomprehension_limit')
    expected message ==> absence of grandpy during << 24 H 00 >> ('exhausted')

---
+++ grandpyRobot.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+#!/usr/bin/env python
+
+from frozendict import frozendict
+
+class GrandpyRobot:
+
+
+    @staticmethod
+    def grandpy_data_of_message(key_data):
+        grandpy_message = frozendict({
+            'home': "Bonjour Mon petit, en quoi puis-je t'aider ?",
+            'user_question': 'As tu une nouvelle question a me demander ?',
+            'response': 'Voici Ta Réponse à la question !',
+            'tired': 'houla, maintenant ma memoire commence a fatiguer !',
+            'incomprehension':\
+                "Ha, Je ne comprends pas, essaye d'être plus précis ... !",
+            'mannerless':\
+                "s'il te plait, reformule ta question en étant plus polis ... !",
+            'disrespectful':\
+                "Hola, sois plus RESPECTUEUX ENVERS TES AINES 'MON PETIT' ... !",
+            'incivility_limit': 'cette impolitesse me FATIGUE ... !',
+            'indecency_limit': 'cette grossierete me FATIGUE ... !',
+            'incomprehension_limit': 'cette incomprehension me FATIGUE ... !',
+            'exhausted': 'je suis fatigué reviens me voir demain !'
+        })
+        return grandpy_message[key_data]
--- test_grandpyRobot.py
+++ test_grandpyRobot.py
@@ -8,10 +8,48 @@ class TestGrandpyRobot:
         self.grandpy = grandpyRobot.GrandpyRobot()
 
     def test_grandpy_data_message(self):
-        expected_response_result =\
-           "Bonjour Mon petit, en quoi puis-je t'aider ?"
-        response_result =\
-            self.grandpy.grandpy_data_of_message('home')
+        expected_response_result = "Bonjour Mon petit, en quoi puis-je t'aider ?"
+        response_result = self.grandpy.grandpy_data_of_message('home')
+        assert expected_response_result == response_result
 
-        assert expected_response_result ==\
-            response_result
+        expected_response_result = 'As tu une nouvelle question a me demander ?'
+        response_result = self.grandpy.grandpy_data_of_message('user_question')
+        assert expected_response_result == response_result
+
+        expected_response_result = 'Voici Ta Réponse à la question !'
+        response_result = self.grandpy.grandpy_data_of_message('response')
+        assert expected_response_result == response_result
+
+        expected_response_result = 'houla, maintenant ma memoire commence a fatiguer !'
+        response_result = self.grandpy.grandpy_data_of_message('tired')
+        assert expected_response_result == response_result
+
+    def test_grandpy_incorrect_message(self):
+        expected_response_result = "Ha, Je ne comprends pas, essaye d'être plus précis ... !"
+        response_result = self.grandpy.grandpy_data_of_message('incomprehension')
+        assert expected_response_result == response_result
+
+        expected_response_result = "s'il te plait, reformule ta question en étant plus polis ... !"
+        response_result = self.grandpy.grandpy_data_of_message('mannerless')
+        assert expected_response_result == response_result
+
+        expected_response_result = "Hola, sois plus RESPECTUEUX ENVERS TES AINES 'MON PETIT' ... !"
+        response_result = self.grandpy.grandpy_data_of_message('disrespectful')
+        assert expected_response_result == response_result
+
+    def test_grandpy_limit_message(self):
+        expected_response_result = 'cette impolitesse me FATIGUE ... !'
+        response_result = self.grandpy.grandpy_data_of_message('incivility_limit')
+        assert expected_response_result == response_result
+
+        expected_response_result = 'cette grossierete me FATIGUE ... !'
+        response_result = self.grandpy.grandpy_data_of_message('indecency_limit')
+        assert expected_response_result == response_result
+
+        expected_response_result = 'cette incomprehension me FATIGUE ... !'
+        response_result = self.grandpy.grandpy_data_of_message('incomprehension_limit')
+        assert expected_response_result == response_result
+
+        expected_response_result = 'je suis fatigué reviens me voir demain !'
+        response_result = self.grandpy.grandpy_data_of_message('exhausted')
+        assert expected_response_result == response_result
