171aed7baa6d9e607832a777c4bde3261b0efbfb
Pascal MACCHI <pascal.macchi@gmail.com>
2021-07-07 09:15:47 +0200

    Refection for indentation in grandpyRobot module

--- grandpyRobot.py
+++ grandpyRobot.py
@@ -1,10 +1,11 @@
 #coding:utf-8
 #!/usr/bin/env python
 """grandpyRobot management module"""
+
 from frozendict import frozendict
 
 class GrandpyRobot:
-     """class for the organization of grandpyRobot according to user behavior"""
+    """class for the organization of grandpyRobot according to user behavior"""
 
     @staticmethod
     def grandpy_data_of_message(key_data):
