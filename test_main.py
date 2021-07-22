dbffe2165307fda075e062ccc207d22e782e0f61
Pascal MACCHI <pascal.macchi@gmail.com>
2021-07-22 09:01:50 +0200

    TDD module test_main.py
    class TestMain
        Creation of the test_number_user_incivility_to_X3 method
    
        test_number_user_incivility_to_X3 method
            Call of the main function 3 times
            to control the incrementation of the user incility counter

--- test_main.py
+++ test_main.py
@@ -1,25 +1,33 @@
 #coding:utf-8
 #!/usr/bin/env python
 
-from main import UserConversation 
+from main import main
 
 class TestMain:
     def setup_method(self):
-        self.correct_behavior = UserConversation('bonjour', db_number=1)
-        self.incorrect_behavior = UserConversation('vieux', db_number=1)
+        self.correct_behavior = main('bonjour', db_number=1)
+        self.incorrect_behavior = main('vieux', db_number=1)
+
+    def tearDown_module(self):
+        self.correct_behavior.user_incivility = True
+        self.correct_behavior.number_user_incivility = 0
 
     def test_user_behavior_after_home_message(self):
         expected_behavior_result = False
-        behavior_result = self.correct_behavior.main()
-        assert expected_behavior_result == behavior_result
+        behavior_result = self.correct_behavior
+        assert expected_behavior_result == behavior_result.user_incivility
 
         expected_behavior_result = True
-        behavior_result = self.incorrect_behavior.main()
-        assert expected_behavior_result == behavior_result
+        behavior_result = self.incorrect_behavior
+        assert expected_behavior_result == behavior_result.user_incivility
 
-    #def test_number_user_incivility_to_X3(self)
-        #number_user_incivility_result = 3
-        
+    def test_number_user_incivility_to_X3(self):
+        expected_number_user_incivility_result = 3
+        self.incorrect_behavior
+        self.incorrect_behavior
+        number_user_incivility_result = self.incorrect_behavior
+        assert expected_number_user_incivility_result ==\
+            number_user_incivility_result.number_user_incivility
 
 
 
