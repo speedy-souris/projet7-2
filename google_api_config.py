ec07fd303c8296d1d5dc8ff79d3fe3ee37e55db0
Pascal MACCHI <pascal.macchi@gmail.com>
2021-08-06 10:14:18 +0200

    TDD module test_google_api_config.py
    class TestGoogleApi
    
        method test_get_geolocated_api_address with the parameter monkeypatch
            mock with the value 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8' and the google map key
            mock with the value 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8' and the google static map key
            mock with the value 'ChIJky12Zn3p9EcREQI4zzdgzbk' and the google map key
    
    Script module google_api_config.py
    
    class GoogleApiData
    
        static method get_settings_for_address_api
            parameters dictionary which contains all the parameters to create the url of the google api
    
        get_address_api_from_placeid method
            google api which determines the place_id with the address argument and the internal key

--- google_api_config.py
+++ google_api_config.py
@@ -22,20 +22,49 @@ class GoogleApiData:
         }
         return parameters
 
+    @staticmethod
+    def get_settings_for_address_api(placeid, key):
+        """determining the localized address for the found placeid"""
+        parameters = {
+            'placeid': f'{placeid}',
+            'fields': 'formatted_address,geometry',
+            'key': f'{key}'
+        }
+        return parameters
+
     def get_placeid_from_address(self, address, key):
         """Google map API place_id search function
-
         Result ok with address to 'openClassrooms'
         {
-           "candidates" : [
+           'candidates': [
               {
-                 "place_id" : "ChIJIZX8lhRu5kcRGwYk8Ce3Vc8"
+                 'place_id': 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8'
               }
            ],
-           "status" : "OK"
+           'status' : 'OK'
         }
         """
         url_api = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
         parameter_data = self.get_settings_for_placeid_api(address, key)
-        placeid_value = self. get_url_from_json(url_api, parameter_data)
+        placeid_value = self.get_url_from_json(url_api, parameter_data)
         return placeid_value
+
+    def get_address_api_from_placeid(self, placeid, key):
+        """Google map API address search with place_id function
+            Result OK with place_id 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8'
+            {
+                'html_attributions': [],
+                'result': {
+                    'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
+                    'geometry': {
+                        'location': {'lat': 48.8975156, 'lng': 2.3833993},
+                        'viewport': {
+                            'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
+                            'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}}}},
+                'status': 'OK'
+            }
+        """
+        url_api = 'https://maps.googleapis.com/maps/api/place/details/json'
+        parameter_data = self.get_settings_for_address_api(placeid, key)
+        address_api_value = self.get_url_from_json(url_api, parameter_data)
+        return address_api_value
--- test_google_api_config.py
+++ test_google_api_config.py
@@ -44,7 +44,7 @@ class TestGoogleApi:
         monkeypatch.setattr(requests, 'get', mockreturn)
         identifier_result = self.google_api_data.get_placeid_from_address('rueopenClassRooms', self.key_map)
         assert identifier_expected == identifier_result
-        
+
         identifier_expected = {
             'candidates' : [],
             'error_message' : 'This API key is not authorized to use this service or API.',
@@ -56,3 +56,50 @@ class TestGoogleApi:
         identifier_result =\
             self.google_api_data.get_placeid_from_address('openClassrooms', self.key_static_map)
         assert identifier_expected == identifier_result
+
+    def test_geolocated_api_adresse(self, monkeypatch):
+        address_api_expected = {
+            'html_attributions': [],
+            'result': {
+                'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
+                'geometry': {
+                    'location': {'lat': 48.8975156, 'lng': 2.3833993},
+                    'viewport': {
+                        'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
+                        'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}}}},
+            'status': 'OK'
+        }
+        mock_result = address_api_expected
+        mockreturn = get_mockreturn(mock_result)
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        address_api_result =\
+            self.google_api_data.get_address_api_from_placeid('ChIJIZX8lhRu5kcRGwYk8Ce3Vc8', self.key_map)
+        assert address_api_expected == address_api_result
+
+        address_api_expected = {
+           'error_message' : 'This API key is not authorized to use this service or API.',
+           'html_attributions' : [],
+           'status' : 'REQUEST_DENIED'
+        }
+        mock_result = address_api_expected
+        mockreturn = get_mockreturn(mock_result)
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        address_api_result =\
+            self.google_api_data.get_address_api_from_placeid('ChIJIZX8lhRu5kcRGwYk8Ce3Vc8', self.key_static_map)
+        assert address_api_expected == address_api_result
+
+        address_api_expected = {
+           'html_attributions': [],
+            'status': 'INVALID_REQUEST'
+        }
+        mock_result = address_api_expected
+        mockreturn = get_mockreturn(mock_result)
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        address_api_result =\
+            self.google_api_data.get_address_api_from_placeid('ChIJky12Zn3p9EcREQI4zzdgzbk', self.key_map)
+        assert address_api_expected == address_api_result
+
+
+
+
+
