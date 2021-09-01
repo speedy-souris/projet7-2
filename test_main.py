413f1380b97041ac6c862f4f6c610bcae5842242
Pascal MACCHI <pascal.macchi@gmail.com>
2021-09-01 09:26:53 +0200

    refection module test_main.py     in all methods after line 9     replaced argument db_number = 1 with db_number = db_number

--- test_main.py
+++ test_main.py
@@ -12,14 +12,14 @@ class TestMain:
     def test_check_user_behavior_after_grandpy_home_message(self):
         has_expected_incivility_behavior = False
         grandpy_code_expected = 'user_question'
-        user_conversation_data = main('bonjour', db_number=1)
+        user_conversation_data = main('bonjour', db_number=db_number)
 
         assert has_expected_incivility_behavior == user_conversation_data.has_user_incivility
         assert grandpy_code_expected == user_conversation_data.grandpy_code
 
         has_an_expected_incivility_behavior = False
         grandpy_code_expected = 'user_question'
-        user_conversation_data = main('ou se trouve Openclassrooms', db_number=1)
+        user_conversation_data = main('ou se trouve Openclassrooms', db_number=db_number)
 
         assert has_an_expected_incivility_behavior == user_conversation_data.has_user_incivility
         assert grandpy_code_expected == user_conversation_data.grandpy_code
@@ -29,8 +29,8 @@ class TestMain:
         grandpy_code_expected = 'incivility_limit'
 
         for counter_user_incivility in range(1,3):
-            main('ou se trouve Openclassrooms', db_number=1)
-        user_conversation_data = main('ou se trouve Openclassrooms', db_number=1)
+            main('ou se trouve Openclassrooms', db_number=db_number)
+        user_conversation_data = main('ou se trouve Openclassrooms', db_number=db_number)
 
         assert number_of_expected_user_incivility == user_conversation_data.number_user_incivility
         assert grandpy_code_expected == user_conversation_data.grandpy_code
@@ -38,10 +38,10 @@ class TestMain:
     def test_count_number_of_user_request_up_to_10(self):
         number_of_expected_user_request = 10
         grandpy_code_expected = 'exhausted'
-        main('bonjour', db_number=1)
+        main('bonjour', db_number=db_number)
         for counter_user_request in range(1,10):
-            main('ou se trouve Openclassrooms', db_number=1)
-        user_conversation_data = main('ou se trouve Openclassrooms', db_number=1)
+            main('ou se trouve Openclassrooms', db_number=db_number)
+        user_conversation_data = main('ou se trouve Openclassrooms', db_number=db_number)
 
         assert number_of_expected_user_request == user_conversation_data.number_user_request
         assert grandpy_code_expected == user_conversation_data.grandpy_code
@@ -49,10 +49,10 @@ class TestMain:
     def test_count_number_of_user_request_equal_to_5(self):
         number_of_expected_user_request = 5
         grandpy_code_expected = 'tired'
-        main('bonjour', db_number=1)
+        main('bonjour', db_number=db_number)
         for counter_user_request in range(1,4):
-            main('ou se trouve Openclassrooms', db_number=1)
-        user_conversation_data = main('ou se trouve Openclassrooms', db_number=1)
+            main('ou se trouve Openclassrooms', db_number=db_number)
+        user_conversation_data = main('ou se trouve Openclassrooms', db_number=db_number)
 
         assert number_of_expected_user_request == user_conversation_data.number_user_request
         assert grandpy_code_expected == user_conversation_data.grandpy_code
@@ -60,10 +60,10 @@ class TestMain:
     def test_count_number_of_user_indecency_up_to_3(self):
         number_of_expected_user_indecency = 3
         grandpy_code_expected = 'indecency_limit'
-        main('bonjour', db_number=1)
+        main('bonjour', db_number=db_number)
         for counter_user_indecency in range(1,3):
-            main('vieux', db_number=1)
-        user_conversation_data = main('vieux', db_number=1)
+            main('vieux', db_number=db_number)
+        user_conversation_data = main('vieux', db_number=db_number)
 
         assert number_of_expected_user_indecency == user_conversation_data.number_user_indecency
         assert grandpy_code_expected == user_conversation_data.grandpy_code
@@ -71,10 +71,10 @@ class TestMain:
     def test_count_number_of_user_incomprehension_up_to_3(self):
         number_of_expected_user_incomprehension = 3
         grandpy_code_expected = 'incomprehension_limit'
-        main('bonjour', db_number=1)
+        main('bonjour', db_number=db_number)
         for counter_user_indecency in range(1,3):
-            main('XXXX', db_number=1)
-        user_conversation_data = main('XXXX', db_number=1)
+            main('XXXX', db_number=db_number)
+        user_conversation_data = main('XXXX', db_number=db_number)
 
         assert number_of_expected_user_incomprehension == user_conversation_data.number_user_incomprehension
         assert grandpy_code_expected == user_conversation_data.grandpy_code
